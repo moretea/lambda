40490b32863d662a7c2095ffa439e9e0c4700a09
Maarten Hoogendoorn <maarten@moretea.nl>
2013-01-18 14:06:45 +0100

    Children & redexes

--- lib/lambda/concepts.rb
+++ lib/lambda/concepts.rb
@@ -1,20 +1,46 @@
 module Lambda
-  class Variable
+  class Concept
+    def redexes
+      self_and_desendants.select { |concept| concept.kind_of?(Application) && concept.is_redex? }
+    end
+
+    def self_and_desendants
+      descendants << self
+    end
+
+    def descendants
+      if children.any?
+        children.collect(&:self_and_desendants).flatten
+      else
+        []
+      end
+    end
+  end
+
+  class Variable < Concept
     attr_reader :name
     def initialize name
       @name = name
     end
+
+    def children
+      []
+    end
   end
 
-  class Abstraction
+  class Abstraction < Concept
     attr_reader :over, :body
     def initialize over, body
       @over = over
       @body = body
     end
+
+    def children
+      [body]
+    end
   end
 
-  class Application
+  class Application < Concept
     attr_reader :left, :right
 
     def initialize left, right
@@ -24,6 +50,10 @@ module Lambda
 
     def is_redex?
       @left.kind_of? Abstraction
+    end
+
+    def children
+      [left, right]
     end
   end
 end
--- spec/concepts_spec.rb
+++ spec/concepts_spec.rb
@@ -1,6 +1,48 @@
 require "lambda"
 
 module Lambda
+  describe Concept do
+    describe "#descendants" do
+      context "of variable" do
+        subject { Variable.new("x") }
+        its(:descendants) { should == [] }
+      end
+
+      context "of abstraction " do
+        let(:over) { Variable.new("x") }
+        let(:body) { Variable.new("y") }
+        subject { Abstraction.new(over, body) }
+        its(:descendants) { should == [body] }
+      end
+
+      context "of applications" do
+        let(:top)         { Application.new(top_left, top_right) }
+        let(:top_left)    { Application.new(other_left, other_right) }
+        let(:top_right)   { Variable.new("p") }
+        let(:other_left)  { Variable.new("q") }
+        let(:other_right) { Variable.new("r") }
+
+        subject { top }
+
+        its(:descendants) { should =~ [top_left, top_right, other_left, other_right] }
+      end
+    end
+
+    context "#redexes" do
+      context "the concept itself" do
+        subject do
+          Application.new(
+            Abstraction.new(Variable.new("x"), Variable.new("x")),
+            Variable.new("y")
+          )
+        end
+
+        it { subject.redexes.should == [subject] }
+
+        its(:redexes) { should be_kind_of Array }
+      end
+    end
+  end
   describe Application do
     context "#is_redex?" do
       subject { Application.new(left, right) }
